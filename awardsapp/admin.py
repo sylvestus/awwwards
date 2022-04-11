7a864c720e8622dfc9966818235fa49ec786c7a6
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-04-11 15:57:52 +0300

    app  admin

---
+++ awardsapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Profile,Ratings,Post
+
+# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Ratings)
+
