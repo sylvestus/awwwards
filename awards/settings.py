61af466988a4f0f6c5c888ab7edb8c6a4899dbe4
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-04-11 17:14:59 +0300

    add heroku configs

--- awards/settings.py
+++ awards/settings.py
@@ -170,7 +170,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -181,10 +180,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL='landingPage'
-LOGOUT_REDIRECT_URL='account/login'
-django_heroku.settings(locals())
+
+
